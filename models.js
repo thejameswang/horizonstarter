3298a4a376ec9c1c8c4f82a56a63ac0b076934e2
James Wang <j.wang5441@gmail.com>
2018-02-01 21:32:43 -0800

    finishes framework for the project

--- models.js
+++ models.js
@@ -32,7 +32,7 @@ var Project = mongoose.model('Project', {
     type: String,
     enum: ['Famous Muppet Frogs','Current Black Presidents','The Pen Is Mightier', 'Famous Mothers', 'Drummers Named Ringo', '1-Letter Words', "Months That Start With 'Feb'", 'How Many Fingers Am I Holding Up', 'Potent Potables'],
     required: true
-  }
+  },
 });
 /*goal: Type: Number, required
 description: Type: String
